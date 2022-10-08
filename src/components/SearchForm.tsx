f8a6674e27278ab1c0f6c53429b56b2fbad5b867
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-10-09 00:10:41 +0200

    Fix focus on search

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -4,14 +4,14 @@ import Search from 'assets/search.svg';
 const SearchForm = () => {
 	return (
 		<>
-			<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+			<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none focus-visible:outline-none">
 				<Search className="font-semibold text-gray-500" />
 			</div>
 			<input
-				className="block w-full p-4 pl-10 text-base text-white bg-gray-700 border border-gray-600 rounded-lg"
-				placeholder="Wyszukiwarka"
+				className="block w-full p-4 pl-10 text-base text-white bg-gray-700 border border-gray-600 rounded-lg focus-visible:outline-none"
+				placeholder="Search"
 			/>
-			<button className="text-white absolute right-2.5 bottom-2.5 bg-primary font-medium rounded-lg text-sm px-4 py-2">
+			<button className="text-white absolute right-2.5 bottom-2.5 bg-primary font-medium rounded-lg text-sm px-4 py-2 focus-visible:outline-none">
 				<Search className="text-white" />
 			</button>
 		</>
