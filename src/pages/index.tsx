c060bf2725f04e10d3bd2e0f5d7ee8b7cda5b062
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-10-09 23:42:21 +0200

    Change page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import UnauthorizedPage from 'templates/UnauthorizedPage';
 
 const Home: NextPage = () => {
 	return (
-		<UnauthorizedPage title={'Strona główna'} content={'Strona główna'}>
+		<UnauthorizedPage title={'Home'} content={'Home'}>
 			<h1>Strona główna</h1>
 		</UnauthorizedPage>
 	);
