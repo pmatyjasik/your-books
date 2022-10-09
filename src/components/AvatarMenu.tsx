f444963adaee49eef27bb62db961e34910e7a3d9
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-10-10 00:50:06 +0200

    Change user dropdown menu

--- src/components/AvatarMenu.tsx
+++ src/components/AvatarMenu.tsx
@@ -4,6 +4,12 @@ import AvatarMenuItemu from 'components/AvatarMenuItem';
 import router from 'next/router';
 import { auth, logout } from '../firebase/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+	BsFillArrowDownCircleFill,
+	BsFillArrowUpCircleFill,
+} from 'react-icons/bs';
+
+import { CgProfile } from 'react-icons/cg';
 
 const AvatarMenu = () => {
 	const wrapperRef = useRef<HTMLDivElement>(null);
@@ -21,11 +27,23 @@ const AvatarMenu = () => {
 
 	return (
 		<div ref={wrapperRef} className="relative" onClick={toggle}>
-			<div className="relative inline-flex items-center justify-center overflow-hidden bg-gray-600 rounded-full w-14 h-14">
-				<span className="font-medium text-white">U</span>
-			</div>
+			{
+				<div className="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden bg-gray-600 rounded-full">
+					<p className="flex items-center text-lg font-medium text-white">
+						<span className="hidden md:block">
+							{user?.displayName ? user?.displayName : ''}
+						</span>
+						<CgProfile className="block w-8 h-8 md:hidden" />
+						{isOpen ? (
+							<BsFillArrowUpCircleFill className="ml-2" />
+						) : (
+							<BsFillArrowDownCircleFill className="ml-2" />
+						)}
+					</p>
+				</div>
+			}
 			{isOpen && (
-				<div className="absolute right-0 z-10 mt-4 origin-top-right rounded-md shadow-lg w-36 bg-main">
+				<div className="absolute right-0 z-10 mt-4 origin-top-right rounded-md shadow-lg w-max md:w-40 bg-main">
 					<div className="py-1 bg-gray-700 border border-gray-600 rounded-lg">
 						<AvatarMenuItemu onClick={() => router.push('/profile')}>
 							Profile
@@ -39,7 +57,9 @@ const AvatarMenu = () => {
 						<AvatarMenuItemu onClick={() => router.push('/settings')}>
 							Settings
 						</AvatarMenuItemu>
-						<AvatarMenuItemu onClick={handleLogout}>Logout</AvatarMenuItemu>
+						<AvatarMenuItemu logout={true} onClick={handleLogout}>
+							Logout
+						</AvatarMenuItemu>
 					</div>
 				</div>
 			)}
