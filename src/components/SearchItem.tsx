1d76b853f53ed22f7400b2e2f81e57d3955f2db9
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-10-15 20:10:55 +0200

    Add void to props

--- src/components/SearchItem.tsx
+++ src/components/SearchItem.tsx
@@ -5,12 +5,13 @@ interface SearchItemProps {
 	title: string;
 	author: string;
 	id: string;
+	onClick?: () => void;
 }
 
-const SearchItem = ({ title, author, id }: SearchItemProps) => {
+const SearchItem = ({ title, author, id, onClick }: SearchItemProps) => {
 	return (
 		<Link href={`/book/${id}`} key={id}>
-			<p className="pl-10 text-xl text-white cursor-pointer">
+			<p className="pl-10 text-xl text-white cursor-pointer" onClick={onClick}>
 				{title}
 				{author}
 			</p>
